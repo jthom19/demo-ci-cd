41bceed2d7f63fe4bc14d55ec8d8e26f0c42c4a9
Jonathan Thom <jonathanthom@Jonathans-MacBook-Pro.local>
2025-07-16 21:26:21 -0400

    Comment out

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def hello():
-    return "Hello, CI/CD!"
+# def hello():
+#     return "Hello, CI/CD!"
 
-if __name__ == "__main__":
-    print(hello())
+# if __name__ == "__main__":
+#     print(hello())
